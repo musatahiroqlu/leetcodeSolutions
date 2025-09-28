e097cf2c4ebc3adac8eddb17620df7e7aefeaa5d
Musa <130700627+musatahiroqlu@users.noreply.github.com>
2025-09-28 13:02:44 +0400

    Update largest-perimeter-triangle.java

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
@@ -1,26 +1,26 @@
 class Solution {
      public static int largestPerimeter(int[] nums) {
-        // Əvvəlcə array-ı artan sıralayaq
+     
         Arrays.sort(nums);
 
-        // Sonda ən böyük üçlüyü yoxlayırıq
+        
         for (int i = nums.length - 1; i >= 2; i--) {
             int a = nums[i];
             int b = nums[i - 1];
             int c = nums[i - 2];
-            // Üçbucaq şərti: c + b > a
+          
             if (b + c > a) {
-                return a + b + c; // perimetr
+                return a + b + c; 
             }
         }
-        return 0; // heç bir üçbucaq mümkün deyilsə
+        return 0; 
     }
 
     public static void main(String[] args) {
         int[] nums1 = {2, 1, 2};
-        System.out.println(largestPerimeter(nums1)); // 5
+        System.out.println(largestPerimeter(nums1));
 
         int[] nums2 = {1, 2, 1, 10};
-        System.out.println(largestPerimeter(nums2)); // 0
+        System.out.println(largestPerimeter(nums2));
     }
 }
